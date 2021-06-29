8b386641e08a39cfccee380c5137aa140c0cbd2d
akshaymunot30 <akshaymunot30@gmail.com>
2021-06-29 15:24:16 +0530

    Add score, and reset quiz button

--- lib/main.dart
+++ lib/main.dart
@@ -1,64 +1,89 @@
 import 'package:flutter/material.dart';
 
-import './question.dart';
-import './answer.dart';
+import './quiz.dart';
+import './result.dart';
 
 void main() => runApp(MyApp());
 
 class MyApp extends StatefulWidget {
   @override
   State<StatefulWidget> createState() {
+    // ignore: todo
     // TODO: implement createState
     return _MyAppState();
   }
 }
 
 class _MyAppState extends State<MyApp> {
-  var _questionIndex = 0;
+  final _questions = const <Map<String, dynamic>>[
+    <String, dynamic>{
+      'questionText': 'What\'s your favourite color?',
+      'answers': [
+        {'text': 'Blue', 'score': 4},
+        {'text': 'Black', 'score': 10},
+        {'text': 'Red', 'score': 6},
+        {'text': 'White', 'score': 2}
+      ],
+    },
+    <String, dynamic>{
+      'questionText': 'What\'s your favourite animal?',
+      'answers': [
+        {'text': 'Lion', 'score': 8},
+        {'text': 'Dog', 'score': 4},
+        {'text': 'Cat', 'score': 4},
+        {'text': 'Wolf', 'score': 10}
+      ],
+    },
+    <String, dynamic>{
+      'questionText': 'Who\'s your favourite instructor?',
+      'answers': [
+        {'text': 'Max', 'score': 8},
+        {'text': 'Tom', 'score': 4},
+        {'text': 'Silus', 'score': 10},
+        {'text': 'John', 'score': 2}
+      ],
+    },
+  ];
 
-  void _answerQuestion() {
+  var _questionIndex = 0;
+  var _totalScore = 0;
+
+  void resetQuiz() {
+    setState(() {
+      _questionIndex = 0;
+      _totalScore = 0;
+    });
+    
+  }
+
+  void _answerQuestion(int score) {
+
+    _totalScore += score;
+
     setState(() {
       _questionIndex = _questionIndex + 1;
     });
 
     print(_questionIndex);
+    if (_questionIndex < _questions.length) {
+      print('We have another question!');
+    }
   }
 
   @override
   Widget build(BuildContext context) {
-    const questions = <Map<String, dynamic>>[
-      <String, dynamic>{
-        'questionText': 'What\'s your favourite color?',
-        'answers': ['Blue', 'Black', 'Red', 'White'],
-      },
-      <String, dynamic>{
-        'questionText': 'What\'s your favourite animal?',
-        'answers': ['Lion', 'Dog', 'Cat', 'Wolf'],
-      },
-      <String, dynamic>{
-        'questionText': 'Who\'s your favourite instructor?',
-        'answers': ['Max', 'Tom', 'Silus', 'John'],
-      },
-    ];
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(
           title: Text('Color App'),
         ),
-        body: Column(
-          children: [
-            Question(
-              questions[_questionIndex]['questionText'],
-            ),
-            // Answer(_answerQuestion),
-            // Answer(_answerQuestion),
-            // Answer(_answerQuestion),
-            // Answer(_answerQuestion),
-            ...(questions[_questionIndex]['answers'] as List<String>).map((answer) {
-              return Answer(_answerQuestion,answer);
-            }).toList()
-          ],
-        ),
+        body: _questionIndex < _questions.length
+            ? Quiz(
+                questions: _questions,
+                answerQuestion: _answerQuestion,
+                questionIndex: _questionIndex,
+              )
+            : Result(_totalScore, resetQuiz),
       ),
     );
   }
---
+++ lib/result.dart
@@ -0,0 +1,46 @@
+import 'package:flutter/material.dart';
+
+class Result extends StatelessWidget {
+  final int resultScore;
+  final void Function() resetHanlder;
+
+  Result(this.resultScore, this.resetHanlder);
+
+  String get resultPhrase {
+    var resultText;
+    if (resultScore <= 8) {
+      resultText = 'You are awesome and Innocent!!';
+    } else if (resultScore <= 12) {
+      resultText = 'You are pretty likable!';
+    } else if (resultScore <= 16) {
+      resultText = 'You are...strange?!';
+    } else {
+      resultText = 'You are so bad!!';
+    }
+    return resultText;
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: double.infinity,
+      margin: EdgeInsets.all(10),
+      child: Center(
+        child: Column(
+          children: [
+            Text(
+              resultPhrase,
+              style: TextStyle(fontSize: 36, fontWeight: FontWeight.bold),
+              textAlign: TextAlign.center,
+            ),
+            TextButton(
+              child: Text('Restart Quiz!'),
+              style: TextButton.styleFrom(primary: Colors.blue),
+              onPressed: resetHanlder,
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
